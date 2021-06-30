d6ac1d5243100ecdb621171c097e2ebc3028840e
yurem <Yuriy.Movchan@gmail.com>
2021-06-30 16:51:13 +0300

    feat: register BC provider at startup

--- opendj-server-legacy/src/main/java/org/opends/server/core/DirectoryServer.java
+++ opendj-server-legacy/src/main/java/org/opends/server/core/DirectoryServer.java
@@ -569,7 +569,6 @@ public final class DirectoryServer
     {
       CLIENT_INIT,
       CORE_CONFIG,
-      BC_FIPS_CRYPTO,
       INIT_CRYPTO,
       ADMIN_BACKEND,
       ADMIN_USERS,
@@ -606,7 +605,6 @@ public final class DirectoryServer
     public InitializationBuilder requireCryptoServices()
     {
       Collections.addAll(subSystemsToInitialize,
-          SubSystem.BC_FIPS_CRYPTO,
           SubSystem.INIT_CRYPTO,
           SubSystem.ADMIN_BACKEND,
           SubSystem.ADMIN_USERS,
@@ -691,9 +689,6 @@ public final class DirectoryServer
         case START_CRYPTO:
           startCryptoServices();
           break;
-        case BC_FIPS_CRYPTO:
-            registerBcProvider();
-            break;
         case USER_PLUGINS:
           startUserPlugin();
           break;
@@ -763,23 +758,6 @@ public final class DirectoryServer
       {
         throw new InitializationException(ERR_CANNOT_INITIALIZE_CRYPTO_MANAGER.get(e.getLocalizedMessage()));
       }
-    }
-
-    private void registerBcProvider() throws InitializationException
-    {
-    	if (!isFips()) {
-    		return;
-    	}
-    	
-    	org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider bouncyCastleProvider = (org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider) java.security.Security.getProvider(org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider.PROVIDER_NAME);
-		if (bouncyCastleProvider == null) {
-			logger.info(INFO_BC_PROVIDER_REGISTER.get());
-
-			bouncyCastleProvider = new org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider();
-			java.security.Security.addProvider(bouncyCastleProvider);
-		} else {
-			logger.info(INFO_BC_PROVIDER_REGISTERED_ALREADY.get());
-		}
     }
 
     private void startCryptoServices() throws InitializationException
@@ -1183,6 +1161,23 @@ public final class DirectoryServer
     }
   }
 
+  private void registerBcProvider() throws InitializationException
+  {
+  	if (!isFips()) {
+  		return;
+  	}
+  	
+  	org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider bouncyCastleProvider = (org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider) java.security.Security.getProvider(org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider.PROVIDER_NAME);
+		if (bouncyCastleProvider == null) {
+			logger.info(INFO_BC_PROVIDER_REGISTER.get());
+
+			bouncyCastleProvider = new org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider();
+			java.security.Security.addProvider(bouncyCastleProvider);
+		} else {
+			logger.info(INFO_BC_PROVIDER_REGISTERED_ALREADY.get());
+		}
+  }
+
   /**
    * Bootstraps the Directory Server by initializing all the necessary
    * structures that should be in place before the configuration may be read.
@@ -1425,6 +1420,7 @@ public final class DirectoryServer
       // The core Directory Server configuration.
       coreConfigManager.initializeCoreConfig();
 
+      registerBcProvider();
       initializeCryptoManager();
 
       rotationPolicyConfigManager = new LogRotationPolicyConfigManager(serverContext);
@@ -5039,6 +5035,7 @@ public final class DirectoryServer
     try
     {
       theDirectoryServer.setEnvironmentConfig(environmentConfig);
+      theDirectoryServer.registerBcProvider();
       theDirectoryServer.bootstrapServer();
       theDirectoryServer.initializeConfiguration();
     }
