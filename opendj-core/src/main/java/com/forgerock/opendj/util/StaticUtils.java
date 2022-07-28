8eecb7d3db65ff371225909e6828eab6b0519064
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2022-07-28 16:27:02 +0300

    chore: move fips util methods to separate class

--- opendj-core/src/main/java/com/forgerock/opendj/util/StaticUtils.java
+++ opendj-core/src/main/java/com/forgerock/opendj/util/StaticUtils.java
@@ -99,12 +99,6 @@ public final class StaticUtils {
         }
     }
 
-	public static final String BC_PROVIDER_NAME = "BC";
-	public static final String BC_FIPS_PROVIDER_NAME = "BCFIPS";
-
-    private static final String BC_GENERIC_PROVIDER_CLASS_NAME = "org.bouncycastle.jce.provider.BouncyCastleProvider";
-	private static final String BC_FIPS_PROVIDER_CLASS_NAME    = "org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider";
-
     /**
      * The default scheduler which should be used when the application does not
      * provide one.
@@ -810,50 +804,9 @@ public final class StaticUtils {
 	}
 
     public static void registerBcProvider(){
-    	if (!isFips()) {
-    		return;
-    	}
-
-    	String providerName = BC_PROVIDER_NAME;
-		String className = BC_GENERIC_PROVIDER_CLASS_NAME;
-
-    	boolean bcFipsProvider = checkBcFipsProvider();
-		if (bcFipsProvider) {
-			logger.info(INFO_BC_FIPS_PROVIDER_REGISTER);
-
-			providerName = BC_FIPS_PROVIDER_NAME;
-			className = BC_FIPS_PROVIDER_CLASS_NAME;
-		} else {
-  			logger.info(INFO_BC_PROVIDER_REGISTER.get());
-		}
-
-		installBCProvider(providerName, className);
-    }
-
-    private static void installBCProvider(String providerName, String providerClassName) {
-    	java.security.Provider bouncyCastleProvider = Security.getProvider(providerName);
-		if (bouncyCastleProvider == null) {
-			try {
-				bouncyCastleProvider = (java.security.Provider) Class.forName(providerClassName).getConstructor().newInstance();
-				java.security.Security.insertProviderAt(bouncyCastleProvider, 1);
-			} catch (ReflectiveOperationException | IllegalArgumentException | SecurityException ex) {
-				logger.error(INFO_BC_PROVIDER_FAILED_TO_CREATE.get());
-			}
-		} else {
-			logger.info(INFO_BC_PROVIDER_REGISTERED_ALREADY.get());
-		}
-	}
-
-    private static boolean checkBcFipsProvider() {
-		try {
-			// Check if there is BC FIPS provider libs
-			Class.forName(BC_FIPS_PROVIDER_CLASS_NAME);
-		} catch (ClassNotFoundException e) {
-			logger.trace(INFO_BC_FIPS_PROVIDER_NOT_EXISTS.get(), e);
-			return false;
-		}
-		
-		return true;
+        try {
+            FipsStaticUtils.registerBcProvider();
+        } catch (NoClassDefFoundError e) {}
 	}
 
 }
